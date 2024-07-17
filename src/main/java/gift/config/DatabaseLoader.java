160b13b10daa45b098b020eb53c0b700fa37d8b7
kuma4188 <psb4188@gmail.com>
2024-07-17 16:02:38 +0900

    옵션까지는 성공함

--- src/main/java/gift/config/DatabaseLoader.java
+++ src/main/java/gift/config/DatabaseLoader.java
@@ -2,9 +2,12 @@ package gift.config;
 
 
 import gift.model.Category;
+import gift.model.Option;
 import gift.model.Product;
 import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,21 +19,42 @@ public class DatabaseLoader {
     @Bean
     public CommandLineRunner initData(ProductRepository repository, CategoryRepository categoryRepository) {
         return args -> {
-            // 데이터베이스에 초기 데이터 삽입
-            repository.save(new Product(2001L , "[기프티콘] BBQ 황금올리브치킨", 20000, "https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/1802204067/B.jpg?315000000" , 1));
-            repository.save(new Product(2001L , "스타벅스 기프트 카드 10000원", 10000, "https://image.istarbucks.co.kr/cardImg/20200818/007633_WEB.png" , 2));
-            repository.save(new Product(2002L , "스타벅스 기프트 카드 30000원", 30000, "https://image.istarbucks.co.kr/cardImg/20200818/007633_WEB.png" , 2));
-            repository.save(new Product(2003L , "스타벅스 기프트 카드 50000원", 50000, "https://image.istarbucks.co.kr/cardImg/20200818/007633_WEB.png" , 2));
-            repository.save(new Product(3001L , "1025 독도 크림 80ml", 25600, "https://roundlab.co.kr/web/product/big/202207/96ab1fe05298e73355fb50b04550226b.jpg" , 3));
-            repository.save(new Product(4001L , "체크남방", 19900, "https://item.elandrs.com/upload/prd/orgimg/032/2004327032_0000001.jpg?w=1020&h=&q=100" , 4));
-            repository.save(new Product(6001L , "인간실격", 8100 , "https://image.yes24.com/goods/1387488/XL" , 6));
-            repository.save(new Product(7001L , "윌슨 NBA  콤프 농구공", 85000 , "https://kr.wilson.com/cdn/shop/products/P_WTB7100XB_view_1_1728x.png?v=1660613945" , 7));
-            repository.save(new Product(8001L , "우마루", 999999 , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRyxSrsBRTPnctpzHYtU3vJggE87JQeSb7A&s" , 8));
-            repository.save(new Product(10001L , "삼성 드럼 세탁기 9kg", 700000 , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT77j3eHCybMMbV-D_xMRz0WPgReRpoSnHprA&s" , 10));
-            repository.save(new Product(10002L , "삼성 양문형 냉장고 846 L", 1490000 , "https://images.samsung.com/kdp/goods/2022/09/05/94432d53-fbf7-4052-a68f-a355e6957783.png?$PD_GALLERY_L_PNG$" , 10));
-            repository.save(new Product(11001L , "춘식이", 9999 , "https://i.namu.wiki/i/GQMqb8jtiqpCo6_US7jmWDO30KfPB2MMvbdURVub61Rs6ALKqbG-nUATj-wNk7bXXWIDjiLHJxWYkTELUgybkA.webp" , 11));
-            repository.save(new Product(11002L , "라이언", 9999 , "https://item.kakaocdn.net/do/a7fd7c0630f8aea8419a565fb2773bbc82f3bd8c9735553d03f6f982e10ebe70" , 11));
-            repository.save(new Product(11003L , "어피치", 9999 , "https://item.kakaocdn.net/do/d9ede575178c59ac8f99a073aa704d99effd194bae87d73dd00522794070855d" , 11));
+            // 공통 옵션 생성
+            Option commonOption = new Option("포장 유무", 100, 0, null); // 수량 100, 가격 0, 실제 상품에 설정될 때 제품 연결 필요
+
+            // 상품 및 옵션 데이터 삽입
+            Product product1 = new Product(2001L, "[기프티콘] BBQ 황금올리브치킨", 20000, "https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/1802204067/B.jpg?315000000", 1);
+            List<Option> options1 = Arrays.asList(
+                new Option("01. [Best] 시어버터 핸드 & 시어 스틱 립 밤", 1, 15000, product1),
+                new Option("02. [Special] 더블 체다 치즈볼", 1, 12000, product1),
+                new Option(commonOption.getName(), commonOption.getQuantity(), commonOption.getPrice(), product1)
+            );
+            product1.setOptions(options1);
+            repository.save(product1);
+
+            Product product2 = new Product(2002L, "스타벅스 기프트 카드 10000원", 10000, "https://image.istarbucks.co.kr/cardImg/20200818/007633_WEB.png", 2);
+            List<Option> options2 = Arrays.asList(
+                new Option("01. [Gift] 스타벅스 아메리카노", 1, 5000, product2),
+                new Option("02. [Gift] 스타벅스 라떼", 1, 5500, product2),
+                new Option(commonOption.getName(), commonOption.getQuantity(), commonOption.getPrice(), product2)
+            );
+            product2.setOptions(options2);
+            repository.save(product2);
+
+//            repository.save(new Product(2001L , "[기프티콘] BBQ 황금올리브치킨", 20000, "https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/1802204067/B.jpg?315000000" , 1));
+//            repository.save(new Product(2001L , "스타벅스 기프트 카드 10000원", 10000, "https://image.istarbucks.co.kr/cardImg/20200818/007633_WEB.png" , 2));
+//            repository.save(new Product(2002L , "스타벅스 기프트 카드 30000원", 30000, "https://image.istarbucks.co.kr/cardImg/20200818/007633_WEB.png" , 2));
+//            repository.save(new Product(2003L , "스타벅스 기프트 카드 50000원", 50000, "https://image.istarbucks.co.kr/cardImg/20200818/007633_WEB.png" , 2));
+//            repository.save(new Product(3001L , "1025 독도 크림 80ml", 25600, "https://roundlab.co.kr/web/product/big/202207/96ab1fe05298e73355fb50b04550226b.jpg" , 3));
+//            repository.save(new Product(4001L , "체크남방", 19900, "https://item.elandrs.com/upload/prd/orgimg/032/2004327032_0000001.jpg?w=1020&h=&q=100" , 4));
+//            repository.save(new Product(6001L , "인간실격", 8100 , "https://image.yes24.com/goods/1387488/XL" , 6));
+//            repository.save(new Product(7001L , "윌슨 NBA  콤프 농구공", 85000 , "https://kr.wilson.com/cdn/shop/products/P_WTB7100XB_view_1_1728x.png?v=1660613945" , 7));
+//            repository.save(new Product(8001L , "우마루", 999999 , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRyxSrsBRTPnctpzHYtU3vJggE87JQeSb7A&s" , 8));
+//            repository.save(new Product(10001L , "삼성 드럼 세탁기 9kg", 700000 , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT77j3eHCybMMbV-D_xMRz0WPgReRpoSnHprA&s" , 10));
+//            repository.save(new Product(10002L , "삼성 양문형 냉장고 846 L", 1490000 , "https://images.samsung.com/kdp/goods/2022/09/05/94432d53-fbf7-4052-a68f-a355e6957783.png?$PD_GALLERY_L_PNG$" , 10));
+//            repository.save(new Product(11001L , "춘식이", 9999 , "https://i.namu.wiki/i/GQMqb8jtiqpCo6_US7jmWDO30KfPB2MMvbdURVub61Rs6ALKqbG-nUATj-wNk7bXXWIDjiLHJxWYkTELUgybkA.webp" , 11));
+//            repository.save(new Product(11002L , "라이언", 9999 , "https://item.kakaocdn.net/do/a7fd7c0630f8aea8419a565fb2773bbc82f3bd8c9735553d03f6f982e10ebe70" , 11));
+//            repository.save(new Product(11003L , "어피치", 9999 , "https://item.kakaocdn.net/do/d9ede575178c59ac8f99a073aa704d99effd194bae87d73dd00522794070855d" , 11));
 
 
             categoryRepository.save(new Category(1, "교환권", "#6c95d1", "https://png.pngtree.com/png-vector/20230330/ourmid/pngtree-red-ticket-icon-vector-illustration-png-image_6674739.png", ""));
---
+++ src/main/java/gift/dto/OptionDTO.java
@@ -0,0 +1,61 @@
+package gift.dto;
+
+public class OptionDTO {
+
+    private Long id;
+    private String name;
+    private int quantity;
+    private int price;
+    private Long productId;
+
+    public OptionDTO() {
+    }
+
+    public OptionDTO(Long id, String name, int quantity, int price, Long productId) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.productId = productId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
